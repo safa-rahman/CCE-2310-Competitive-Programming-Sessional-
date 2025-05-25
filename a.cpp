63d4b7bc1990753c2e38c320ab4130d5d9d3ca80
safa-rahman <rsafa7965@gmail.com>
2025-05-25 22:58:14 +0600

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,14 @@
+//Problem Name: Say Hello With C++
+//Course Title: Competitive Programming Sessional
+//Course Code: CCE-2310
+//Course Teacher Name: Mirza Raquib
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+long long a,b;
+char c;
+double d,e;
+cin>>a>>b>>c>>d>>e;
+cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<e<<endl;
+}
+
